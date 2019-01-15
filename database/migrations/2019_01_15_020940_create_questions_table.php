ad1b1d710eb10eb14cda3d85008598b6d6e194e7
Christopher Crowd <christophercrowd@Christophers-Mac-mini.local>
2019-01-14 21:22:41 -0500

    create migrations

--- database/migrations/2019_01_15_020940_create_questions_table.php
+++ database/migrations/2019_01_15_020940_create_questions_table.php
@@ -15,6 +15,12 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
