ff15566b768e1a86d1691f4d6ad744a0b7884953
Christopher Crowd <christophercrowd@Christophers-Mac-mini.local>
2019-01-15 12:32:13 -0500

    likes system

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@LikeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
