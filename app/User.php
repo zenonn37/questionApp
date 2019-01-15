ab5347cb1d70f62f6750f13b1c6f8968e3a47f0e
Christopher Crowd <christophercrowd@Christophers-Mac-mini.local>
2019-01-15 14:12:32 -0500

    JWT

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -28,7 +29,24 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function question(){
+    public function question()
+    {
         return $this->hasMany(Question::class);
+    }
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
