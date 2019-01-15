7428c34ae822991c5ce863b365ed3f6e2ffbf20e
Christopher Crowd <christophercrowd@Christophers-Mac-mini.local>
2019-01-15 10:21:20 -0500

    added question resource

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -8,9 +8,17 @@ use App\User;
 
 class Question extends Model
 {
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+    //protected $guarded = [];
     public function user()
     {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function replies()
@@ -21,5 +29,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +12,6 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/question', 'QuestionController');
