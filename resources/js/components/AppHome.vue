5c9b9d3edb40831673a2fcc3709a39fc9bb3387b
Christopher Crowd <christophercrowd@Christophers-Mac-mini.local>
2019-01-15 16:12:12 -0500

    added vueify

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <tool-bar></tool-bar>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import ToolBar from "./ToolBar.vue";
+import AppFooter from "./AppFooter.vue";
+export default {
+  components: {
+    ToolBar,
+    AppFooter
+  }
+};
+</script>
+
+<style>
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,12 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+           
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+     
+
 
         <title>Laravel</title>
 
